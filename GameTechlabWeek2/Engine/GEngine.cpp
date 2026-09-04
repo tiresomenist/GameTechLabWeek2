646f9d85ad810cea4f9c0884657500058c39c7a1
Kim Gyeongjae <skyneon77@gmail.com>
2026-09-04 21:05:34 +0900

    Premake 추가 및 GEngine 간단 구현

--- GameTechlabWeek2/Engine/GEngine.cpp
+++ GameTechlabWeek2/Engine/GEngine.cpp
@@ -1,2 +1,39 @@
 #include "GEngine.h"
+#include "Windows.h"
 
+float GetTime()
+{
+	LARGE_INTEGER currentTime;
+	QueryPerformanceCounter(&currentTime);
+
+	return static_cast<float>(currentTime.QuadPart);
+}
+
+
+GEngine* GEngine::GetInstance()
+{
+	static GEngine* Engine = new GEngine();
+	return Engine;
+}
+
+void GEngine::Initialize()
+{
+	LastTickTime = GetTime();
+}
+
+void GEngine::Tick()
+{
+	float DeltaTime = GetTime() - LastTickTime;
+
+	// 게임 로직을 수행합니다.
+	// GSceneManager.Update(DeltaTime);
+
+	// 게임 화면을 렌더링합니다.
+	// UScene* CurrentScene = GSceneManager->GetScene();
+	// FRenderer.Render(CurrentScene);
+}
+
+void GEngine::Destroy()
+{
+	// TODO: 정리 로직
+}
