c4f7d6406cc808c276394909011079ac4b0196ca
Kim Gyeongjae <skyneon77@gmail.com>
2026-09-05 17:43:42 +0900

    Scene 구조 개선

--- GameTechlabWeek2/Engine/GSceneManager.cpp
+++ GameTechlabWeek2/Engine/GSceneManager.cpp
@@ -1,2 +1,17 @@
 #include "GSceneManager.h"
 
+void GSceneManager::Initialize()
+{
+}
+
+void GSceneManager::Start()
+{
+}
+
+void GSceneManager::Tick()
+{
+}
+
+void GSceneManager::End()
+{
+}
--- GameTechlabWeek2/Engine/GSceneManager.h
+++ GameTechlabWeek2/Engine/GSceneManager.h
@@ -1,8 +1,29 @@
 #pragma once
 
+class UScene;
 
 // Singleton
 class GSceneManager
 {
+public:
+	static GSceneManager* GetInstance();
+
+	void Initialize();
+
+	void Start();
+	void Tick();
+	void End();
+
+	void ChangeLevel();
+
+private:
+
+	UScene* CurrentScene = nullptr;
+
+	// 싱글톤
+	GSceneManager() = default;
+	~GSceneManager() = default;
+	GSceneManager(const GSceneManager&) = delete;
+	GSceneManager& operator=(const GSceneManager&) = delete;
 };
 
